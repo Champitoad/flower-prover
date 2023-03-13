ebf5b7328b8e409ff31acb57d8dada386002287a
Pablo Donato <pablo.donato@tuta.io>
2023-03-13 23:58:58 +0100

    Add Justify action (down pollination rule!)

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,24 @@
 module Main exposing (..)
 
+import Utils.List
+
+import Browser
+
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import Element.Events as Events
 
 
 -- MAIN
 
 
-main = 
-  layout []
-    (view init)
+main =
+  Browser.sandbox
+    { init = init
+    , update = update
+    , view = \model -> layout [] (view model) }
 
 
 -- MODEL
@@ -21,21 +28,63 @@ type Flower
   = Atom String
   | Flower Garden (List Garden)
 
+type alias Bouquet
+  = List Flower
 
 type Garden
-  = Garden (List Flower)
+  = Garden Bouquet
 
 
-type Zipper
-  = Hole
-  | Bouquet (List Flower) Zipper (List Flower)
-  | Pistil Zipper (List Garden)
-  | Petal Garden (List Garden) Zipper (List Garden)
+type Zip
+  = Bouquet Bouquet Bouquet
+  | Pistil (List Garden)
+  | Petal Garden (List Garden) (List Garden)
+
+type alias Zipper
+  = List Zip
+
+-- We encode zippers as lists, where the head is the innermost context
+
+fillZip : Zip -> Bouquet -> Bouquet
+fillZip zip bouquet =
+  case zip of
+    Bouquet left right ->
+      left ++ bouquet ++ right
+
+    Pistil petals ->
+      [Flower (Garden bouquet) petals]
+
+    Petal pistil left right ->
+      [Flower pistil (left ++ Garden bouquet :: right)]
+
+
+fillZipper : Bouquet -> Zipper -> Bouquet
+fillZipper =
+  List.foldl fillZip
+
+
+hypsZip : Zip -> Bouquet
+hypsZip zip =
+  case zip of
+    Bouquet left right ->
+      left ++ right
+
+    Pistil _ ->
+      []
+    
+    Petal (Garden bouquet) _ _ ->
+      bouquet
+
+
+hypsZipper : Zipper -> Bouquet
+hypsZipper zipper =
+  List.foldl (\zip acc -> hypsZip zip ++ acc) [] zipper
 
 
 type Polarity
   = Pos
   | Neg
+
 
 negate : Polarity -> Polarity
 negate polarity =
@@ -47,38 +96,52 @@ negate polarity =
 type alias Context =
   { zipper : Zipper,
     polarity : Polarity }
-
+ 
 
 type alias Model
-  = Flower
+  = Bouquet
 
 
 init : Model
 init =
-  Flower
-    ( Garden
-        [ Atom "a"
-        , Flower
-            ( Garden
-                [ Atom "a" ] )
-            [ Garden
-                [ Atom "b" ],
-              Garden
-                [ Flower
-                    ( Garden
-                        [ Atom "b" ] )
-                    [ Garden
-                        [ Atom "c" ] ],
-                  Atom "b" ] ]
-        , Atom "d" ] )
-    [ Garden
-        [ Atom "b"
-        , Atom "a" ]
-    , Garden
-      [ Atom "c" ] ]
+  [ Flower
+      ( Garden
+          [ Atom "a"
+          , Flower
+              ( Garden
+                  [ Atom "a" ] )
+              [ Garden
+                  [ Atom "b" ],
+                Garden
+                  [ Flower
+                      ( Garden
+                          [ Atom "b" ] )
+                      [ Garden
+                          [ Atom "c" ] ],
+                    Atom "b" ] ]
+          , Atom "d" ] )
+      [ Garden
+          [ Atom "b"
+          , Atom "a" ]
+      , Garden
+        [ Atom "c" ] ] ]
 
 
 -- UPDATE
+
+
+type Msg
+  = Justify Zipper Flower
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Justify zipper flower ->
+      if List.member flower (hypsZipper zipper) then
+        fillZipper [] zipper
+      else
+        model
 
 
 -- VIEW
@@ -138,32 +201,47 @@ borderWidth =
   3
 
 
-viewFlower : Context -> Flower -> Element msg
+viewFlower : Context -> Flower -> Element Msg
 viewFlower context flower =
   case flower of
     Atom name ->
-      el [ width fill ]
+      el
+      [ width fill
+      , height fill ]
         (el
           [ centerX
+          , centerY
+          , padding 3
+          -- , Border.width 1
+          , Border.dashed
+          , Border.color (fgColor context.polarity)
           , Font.color (fgColor context.polarity)
-          , Font.size 32 ]
+          , Font.size 32
+          , Events.onClick (Justify context.zipper flower) ]
           (text name))
     
     Flower pistil petals ->
       let
         pistilEl =
-          viewGarden ({ context | polarity = negate context.polarity }) pistil
+          viewGarden
+            { context
+            | zipper = Pistil petals :: context.zipper
+            , polarity = negate context.polarity }
+            pistil
         
         petalsEl =
           let
-            petalEl petal =
-              (viewGarden context petal)
+            petalEl (left, right) petal =
+              viewGarden
+                { context
+                | zipper = Petal pistil left right :: context.zipper }
+                petal
           in
           row
             [ width fill
             , height fill
             , spacing borderWidth ]
-            (List.map petalEl petals)
+            (Utils.List.zipMap petalEl petals)
       in
       column
         [ width fill
@@ -174,18 +252,25 @@ viewFlower context flower =
         [ pistilEl, petalsEl ]
 
 
-viewGarden : Context -> Garden -> Element msg
+viewGarden : Context -> Garden -> Element Msg
 viewGarden context (Garden bouquet) =
+  let
+    flowerEl (left, right) =
+      viewFlower
+        { context
+        | zipper = Bouquet left right :: context.zipper }
+  in
   wrappedRow
     [ width fill
     , height fill
     , padding 20
     , spacing 40
     , Background.color (bgColor context.polarity) ]
-    (List.map (viewFlower context) bouquet)
+    (Utils.List.zipMap flowerEl bouquet)
 
 
-view : Model -> Element msg
+view : Model -> Element Msg
 view model =
   -- text (viewFlowerText model)
-  viewFlower (Context Hole Pos) model
+  column [width fill, height fill]
+    (List.map (viewFlower (Context [] Pos)) model)
