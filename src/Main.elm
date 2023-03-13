c01829f7913047cfdc21ef84d1e6e5b4954f5bf6
Pablo Donato <pablo.donato@tuta.io>
2023-03-13 03:02:57 +0100

    Basic flower type + text view

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,72 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Html
+import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
-main =
-    Html.text "Hello world!"
+-- MAIN
+
+
+main = 
+  Element.layout []
+    (view init)
+
+
+-- MODEL
+
+
+type Flower
+  = Atom String
+  | Flower Garden (List Garden)
+
+
+type Garden
+  = Garden (List Flower)
+
+
+type alias Model
+  = Flower
+
+
+init : Model
+init =
+  Flower (Garden [Atom "a"]) [Garden [Atom "a", Atom "b"], Garden [Atom "c"]]
+
+
+-- UPDATE
+
+
+-- VIEW
+
+
+viewFlowerText : Flower -> String
+viewFlowerText flower =
+  case flower of
+    Atom name ->
+      name    
+    
+    Flower pistil petals ->
+      let
+        pistilText =
+          viewGardenText pistil
+
+        petalsText =
+          petals |>
+          List.map viewGardenText |>
+          String.join "; "
+      in
+      pistilText ++ " ⫐ " ++ petalsText
+
+
+viewGardenText : Garden -> String
+viewGardenText (Garden bouquet) =
+  bouquet |>
+  List.map viewFlowerText |>
+  String.join ", "
+
+
+view : Model -> Element msg
+view model =
+  Element.text (viewFlowerText model)
